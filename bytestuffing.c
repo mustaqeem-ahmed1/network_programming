506c8a192dcd3c0c65de0c008a79cd5a5079741a
mustaqeem-ahmed1 <102944300+mustaqeem-ahmed1@users.noreply.github.com>
2022-07-07 21:45:03 +0530

    Add files via upload

---
+++ bytestuffing.c
@@ -0,0 +1,47 @@
+#include<stdio.h>        	 
+#include<string.h>    	 
+int main(){
+    	char a[20],b[20];
+    	int i,n,j;
+    	char f,s;
+    	printf("Enter the size of the frame : ");
+    	n=0;
+    	scanf("%d",&n);
+    	printf("\nEnter the characters in frame : \n");
+    	for(i=0;i<=n;i++)
+            	scanf("%c",&a[i]);
+    	printf("\n FRAME \n ");
+    	for(i=0;i<=n;i++)
+            	printf("%c",a[i]);
+    	j=0;
+    	for(i=0;i<=n;i++)
+    	{
+            	if(a[i]=='f')
+            	{
+                    	b[j]='s';
+                    	j++;
+                    	b[j]=a[i];
+
+            	}
+            	else if(a[i]=='s')
+            	{
+                    	b[j]='s';
+                    	j++;
+                    	b[j]=a[i];
+            	}
+            	else
+                    	b[j]=a[i];
+
+            	j++;
+    	}
+    	printf("\n RESULT \n");
+   	 
+   	printf("f "); for(i=0;i<j;i++)
+    	{
+           	// printf("\n");
+            	printf("%c ",b[i]);
+    	}
+      	 
+    	printf("f");
+return 0;
+}
